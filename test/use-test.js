d3a2480bd17d91a7df68266766266944e12fd661
Ilya Petrov <ilya.muromec@gmail.com>
2014-08-08 18:00:40 +0300

    Fix both explicit and implicit tags for submodel

--- test/use-test.js
+++ test/use-test.js
@@ -45,6 +45,28 @@ describe('asn1.js models', function() {
       assert.deepEqual(back, data);
 
     });
+
+    it('should honour explicit tag from parent', function() {
+      var SubModel = asn1.define('SubModel', function() {
+        this.seq().obj(
+          this.key('x').octstr()
+        )
+      });
+      var Model = asn1.define('Model', function() {
+        this.seq().obj(
+          this.key('a').int(),
+          this.key('sub').use(SubModel).explicit(0)
+        );
+      });
+
+      var data = {a: 1, sub: {x: new Buffer("123")}};
+      var wire = Model.encode(data, 'der');
+      assert.equal(wire.toString('hex'), '300c020101a00730050403313233');
+      var back = Model.decode(wire, 'der');
+      assert.deepEqual(back, data);
+
+    });
+
   });
 });
 

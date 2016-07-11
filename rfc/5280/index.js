94a5a245c222b25a9f3e5252bc6f238060677f9e
Fedor Indutny <fedor@indutny.com>
2016-07-10 23:35:08 -0400

    5280: someone is having a bad day
    This is not required anymore.

--- rfc/5280/index.js
+++ rfc/5280/index.js
@@ -881,5 +881,3 @@ var x509Extensions = {
   authorityInformationAccess: AuthorityInfoAccessSyntax,
   subjectInformationAccess: SubjectInformationAccess
 };
-
-exports['id-pe-authorityInfoAccess'] = [ 1, 3, 6, 1, 5, 5, 7, 1, 1];
